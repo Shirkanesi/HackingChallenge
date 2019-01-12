12e36284f00b41987865b538a1d61a6dc569e26d
Shirkanesi <keckjulian.rintheim@gmail.com>
2019-01-12 09:28:12 +0100

    Adding Layout.

---
+++ 0x00/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hacking Challenge 0x00</title>
+  </head>
+  <body>
+    <img src="data/img/bg.jpg" alt="" id="background"/>
+    <div id="backCover">.</div>
+    <div id="contentContainer">
+      <div class="article">
+        1.  Guten Tab...<br /><br /><br />
+        cdfghjkiopü
+      </div>
+      <div class="article">
+        1.  Guten Tab...<br /><br /><br />
+        cdfghjkiopü
+      </div>
+      <div class="article">
+        1.  Guten Tab...<br /><br /><br />
+        cdfghjkiopü
+      </div>
+    </div>
+  </body>
+</html>
